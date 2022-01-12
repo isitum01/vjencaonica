0ba3b2c21c3c36cfd2b5c46b8afc9ad9161da704
isitum01 <isitum01@gmail.com>
2022-01-12 21:40:44 +0100

    Configured creating new tables (test table, delete after adding real table)

--- wp-content/plugins/vjencaonica-plugin/includes/class-vjencaonicaplugin-activator.php
+++ wp-content/plugins/vjencaonica-plugin/includes/class-vjencaonicaplugin-activator.php
@@ -8,7 +8,7 @@ class VjencaonicaPlugin_Activator
      * Database version
      * Should be updated every time we modify the database
      */
-    const DB_VERSION = '0.0.1';
+    const DB_VERSION = '0.0.2';
 
     /**
      * Database version option name
@@ -28,7 +28,33 @@ class VjencaonicaPlugin_Activator
             return;
         }
 
+        // Get queries
+        $queries = static::get_queries();
+
+        // Execute queries
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($queries);
+
         // Update the version option in database
         update_option(static::DB_VERSION_OPTION_NAME, static::DB_VERSION);
+    }
+
+    public static function get_queries()
+    {
+        global $wpdb;
+        $wp_prefix          = $wpdb->prefix;
+        $charset_collate    = $wpdb->get_charset_collate();
+
+        // Get table names
+        $test_table_name = "{$wp_prefix}test_table_name";
+
+        return [
+            "CREATE TABLE {$test_table_name}(
+                id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+                first_name VARCHAR(64),
+                last_name VARCHAR(64),
+                PRIMARY KEY id (id)
+            ){$charset_collate};"
+        ];
     }
 }
