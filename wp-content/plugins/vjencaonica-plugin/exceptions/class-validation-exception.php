6066121283bed9f8d8a77cbb646c3fc85153d58e
isitum01 <isitum01@gmail.com>
2022-01-26 00:09:47 +0100

    Controller, Service, Repository and Class included in plugin, added basic exception class

---
+++ wp-content/plugins/vjencaonica-plugin/exceptions/class-validation-exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vjencaonica;
+
+/**
+ * Base template class for validation exception.
+ *
+ * Class Validation_Exception
+ * @package Vjencaonica
+ */
+class Validation_Exception extends \Exception {
+
+}
