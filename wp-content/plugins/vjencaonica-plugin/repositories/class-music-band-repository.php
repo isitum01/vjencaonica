6e7de4adab9f960aa46d34e718ad59bf8d828bd6
isitum01 <isitum01@gmail.com>
2022-01-25 23:54:59 +0100

    Music band repository

--- wp-content/plugins/vjencaonica-plugin/repositories/class-music-band-repository.php
+++ wp-content/plugins/vjencaonica-plugin/repositories/class-music-band-repository.php
@@ -9,7 +9,8 @@ use WpHelpers\Repositories\ARepository;
  * Class Music_Band_Repository
  * @package Vjencaonica
  */
-class Music_Band_Repository extends ARepository {
+class Music_Band_Repository extends ARepository
+{
 
 	/**
 	 * Music_Band_Repository constructor.
@@ -18,8 +19,9 @@ class Music_Band_Repository extends ARepository {
 	 *
 	 * @throws \Exception
 	 */
-	public function __construct( $table_name = 'music-bands' ) {
-		parent::__construct( $table_name );
+	public function __construct()
+	{
+		parent::__construct('vj_music_band_registration');
 	}
 
 	/**
@@ -28,69 +30,54 @@ class Music_Band_Repository extends ARepository {
 	 * @return bool|Music_Band
 	 * @throws \Exception
 	 */
-	public function get_by_id( $id ) {
-		$post = get_post( $id );
-		if ( empty( $post ) ) {
-			return false;
-		}
-
-		return $this->get_by_wp_post( $post );
+	public function get_by_id($id)
+	{
+		return $this->_get_single_by_field('id', intval($id), '%d');
 	}
 
 	/**
-	 * Gets single by wp post.
-	 *
-	 * @param $post
-	 *
-	 * @return bool|Music_Band
-	 * @throws \Exception
-	 */
-	public function get_by_wp_post( $post ) {
-		if ( ! in_array( $post->post_type, [
-			Music_Band::POST_TYPE
-		] ) ) {
-			throw new \Exception( 'post_type is not music-band!' );
-		}
-
-		return $this->_get_single_by_field( 'id', $post->ID, '%d', $post );
-	}
-
-	/**
-	 * Save music band.
+	 * Save Music band.
 	 *
 	 * @param Music_Band $music_band
 	 *
 	 * @return Music_Band
 	 * @throws \Exception
 	 */
-	public function save( $music_band ) {
-		$db_data = $this->get_db_data( $music_band );
+	public function save($music_band)
+	{
+		$db_data = $this->get_db_data($music_band);
+		$db_row = $this->get_db_row($music_band->id);
 
-		if ( empty( $music_band->id ) ) {
-			throw new \Exception( 'music_band id is empty!' );
+		if (empty($music_band->id)) {
+			throw new \Exception('music_band id is empty!');
 		}
 
-		if ( $this->exists( $music_band->id ) ) {
-			// Remove id from values and formats
-			array_shift( $db_data['values'] );
-			array_shift( $db_data['formats'] );
+		if (!empty($db_row)) {
+			// Update in the db
 			$res = $this->db->update(
 				$this->table_name,
 				$db_data['values'],
-				[ 'id' => $music_band->id ],
+				['id' => $music_band->id],
 				$db_data['formats'],
-				[ '%d' ]
+				'%d'
 			);
+
+			// Check if updated
+			if ($res === false) {
+				throw new \Exception('Ketchup_Gang_Registration UPDATE failed!');
+			}
 		} else {
+			$db_data['formats'][] = '%d';
+			// Insert into db
 			$res = $this->db->insert(
 				$this->table_name,
-				$db_data['values'],
-				$db_data['formats']
+				$db_data['values']
 			);
-		}
 
-		if ( $res === false ) {
-			throw new \Exception( 'Save failed!' );
+			// Check if insert failed
+			if ($res === false) {
+				throw new \Exception('Ketchup_Gang_Registration CREATE failed!');
+			}
 		}
 
 		return $music_band;
@@ -101,31 +88,31 @@ class Music_Band_Repository extends ARepository {
 	 *
 	 * @return array
 	 */
-	private function get_db_data( $music_band ) {
+	private function get_db_data($music_band)
+	{
 		$db_data = new Db_Data();
-		$mb_granted = empty($music_band->granted) ? false : $music_band->granted;
 
-		$db_data->add_data( 'id', $music_band->id, '%d' );
-		$db_data->add_data( 'band_name', $music_band->band_name, '%s' );
-		$db_data->add_data( 'phone', $music_band->phone, '%s' );
-		$db_data->add_data( 'email', $music_band->email, '%s' );
-		$db_data->add_data( 'city', $music_band->city, '%s' );
-		$db_data->add_data( 'country', $music_band->country, '%s' );
-		$db_data->add_data( 'available_locations', $music_band->available_locations, '%s' );
-		$db_data->add_data( 'members', $music_band->members, '%s' );
-		$db_data->add_data( 'instruments', $music_band->instruments, '%s' );
-		$db_data->add_data( 'video_link', $music_band->video_link, '%s' );
-		$db_data->add_data( 'genres', $music_band->genres, '%s' );
-		$db_data->add_data( 'female_vocal', $music_band->female_vocal, '%s' );
-		$db_data->add_data( 'male_vocal', $music_band->male_vocal, '%s' );
-		$db_data->add_data( 'website', $music_band->website, '%s' );
-		$db_data->add_data( 'instagram', $music_band->instagram, '%s' );
-		$db_data->add_data( 'facebook', $music_band->facebook, '%s' );
-		$db_data->add_data( 'tags', $music_band->tags, '%s' );
-		$db_data->add_data( 'year_of_foundation', $music_band->year_of_foundation, '%s' );
-		$db_data->add_data( 'description', $music_band->description, '%s' );
-		$db_data->add_data( 'granted', $mb_granted, '%s' );
-		$db_data->add_data( 'date_created', $music_band->date_created, '%s' );
+		$db_data->add_data('id', $music_band->id, '%d');
+		$db_data->add_data('band_name', $music_band->band_name, '%s');
+		$db_data->add_data('phone', $music_band->phone, '%s');
+		$db_data->add_data('email', $music_band->email, '%s');
+		$db_data->add_data('city', $music_band->city, '%s');
+		$db_data->add_data('country', $music_band->country, '%s');
+		$db_data->add_data('available_locations', $music_band->available_locations, '%s');
+		$db_data->add_data('members', $music_band->members, '%s');
+		$db_data->add_data('instruments', $music_band->instruments, '%s');
+		$db_data->add_data('video_link', $music_band->video_link, '%s');
+		$db_data->add_data('genres', $music_band->genres, '%s');
+		$db_data->add_data('female_vocal', $music_band->female_vocal, '%s');
+		$db_data->add_data('male_vocal', $music_band->male_vocal, '%s');
+		$db_data->add_data('website', $music_band->website, '%s');
+		$db_data->add_data('instagram', $music_band->instagram, '%s');
+		$db_data->add_data('facebook', $music_band->facebook, '%s');
+		$db_data->add_data('tags', $music_band->tags, '%s');
+		$db_data->add_data('year_of_foundation', $music_band->year_of_foundation, '%s');
+		$db_data->add_data('description', $music_band->description, '%s');
+		$db_data->add_data('granted', $music_band->granted, '%s');
+		$db_data->add_data('date_created', $music_band->date_created->format(DATE_ATOM), '%s');
 
 		return $db_data->get_data();
 	}
@@ -142,21 +129,20 @@ class Music_Band_Repository extends ARepository {
 	 *
 	 * @return          mixed
 	 */
-	protected function _construct_object( $table_row, $object_data = null ) {
-		return new Music_Band( $table_row, $object_data );
+	protected function _construct_object($table_row, $object_data = null)
+	{
+		return new Music_Band($table_row, $object_data);
 	}
 
 	/**
-	 * Checks if type exists.
+	 * @param int $id
 	 *
-	 * @param $id
-	 *
-	 * @return bool
+	 * @return array|null|object
 	 */
-	private function exists( $id ) {
-		$query = $this->db->prepare( "SELECT * FROM {$this->table_name} WHERE id = %d", intval( $id ) );
-		$result = $this->db->get_row( $query, ARRAY_A );
+	private function get_db_row($id)
+	{
+		$query = $this->db->prepare("SELECT * FROM {$this->table_name} WHERE id = %d", intval($id));
 
-		return ! empty( $result );
+		return $this->db->get_row($query, ARRAY_A);
 	}
 }
